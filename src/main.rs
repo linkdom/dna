7234b6c21a9c99477206885330907ff92901f03a
linkdom <Vorbild94@gmail.com>
2023-07-03 22:35:56 +0200

    fixed rust-analyzer and append and pop_front function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,4 @@
-mod stack;
-use stack::Stack;
+mod singly_linked_list;
 
 fn main() {
-    let mut stack = Stack::new();
-    stack.push(1);
-    stack.push(2);
-    stack.push(3);
-    
-    println!("Top element is: {:?}", stack.peek());
-    println!("Stack size is: {}", stack.size());
-    
-    println!("Popped element: {:?}", stack.pop());
-    println!("Top element is: {:?}", stack.peek());
-    println!("Stack size is: {}", stack.size());
 }
--- src/singly_linked_list/mod.rs
+++ src/singly_linked_list/mod.rs
@@ -15,7 +15,34 @@ impl<T> LinkedList<T> {
     }
 
     pub fn append(*mut self, value: T) {
-        
+        let new_node = Box::new(Node {value, next: None});
+
+        let raw_node = Box::into_raw(new_node);
+
+        unsafe {
+
+            if let Some(tail) = self.tail {
+                (*tail).next = Some(Box::from_raw(raw_node));
+            } else {
+                self.head = Some(Box::from_raw(raw_node));
+            }
+
+            self.tail = Some(raw_node);
+        }
+    }
+
+    pub fn pop_front(&mut self) -> Option<T> {
+        self.head.take().map(|old_head| {
+            let old_head = *old_head;
+            self.head = old_head.next;
+
+            if self.head.is_none() {
+                self.tail = None;
+            }
+
+            old_head.value
+        })
     }
 
 }
+
